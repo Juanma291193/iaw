6bc16d6eaf3d9a02900e42b47e8c074eb9a7067d
Juanma291193 <juanma.roman93@gmail.com>
2024-02-29 09:23:56 +0100

    modified:   estructuraif.php 	new file:   estructuraswitch.php

--- estructuraif.php
+++ estructuraif.php
@@ -7,17 +7,26 @@
 </head>
 <body>
     <?php
-        $x = 34;
-        $y = 2;
+        $x = 33;
+        $y = 3;
 
         if ($x > $y) {
-            echo "<h1> $x es mayor que $y </h1>".'<br>';
+            echo "<h1> $x es mayor que $y . </h1>".'<br>';
         } elseif ($x < $y) {
-            echo "<h1> $x es menor que $y </h1>".'<br>';
+            echo "<h1> $x es menor que $y . </h1>".'<br>';
         } else {
             echo "<h1> $x es igual a $y </h1>".'<br>';
         }
 
+        echo "---------------------------------------------";
+
+        if (($x * $y) % 2 == 0) {
+            echo "<h2> $x por $y da un resultado par.</h2>". $x * $y .'<br>';
+        } else {
+            echo "<h2> $x por $y NO da un resultado par.</h2>". $x * $y .'<br>';
+        }
+
+        echo "---------------------------------------------";
     ?>
 </body>
 </html>
