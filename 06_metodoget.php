7782155bb71cc98b95f7db9960e537c655b70567
Juanma291193 <juanma.roman93@gmail.com>
2024-03-05 12:42:11 +0100

    new file:   06_formularioget.php 	new file:   06_metodoget.php 	new file:   07_formulariopost.php 	new file:   07_metodopost.php

---
+++ 06_metodoget.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action= "06_formularioget.php" method="get">
+        Usuario: <input type ="text" name="usuario"/>
+        Departamento: <input type ="text" name="departamento"/>
+        <br>
+        <input type ="submit" name="Continuar"/>
+        <input type ="reset" name="Borrar"/>
+    </form>
+</body>
+</html>
---
+++ 07_metodopost.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action= "07_formulariopost.php" method="post">
+        Nombre: <input type ="text" name="nombre"/>
+        <br>
+        Edad: <input type ="text" name="edad"/>
+        <br>
+        Nº de horas trabajadas en la semana: <input type ="text" name="horas"/>
+        <br>
+        <input type ="submit" name="Continuar"/>
+        <input type ="reset" name="Borrar"/>
+</body>
+</html>
